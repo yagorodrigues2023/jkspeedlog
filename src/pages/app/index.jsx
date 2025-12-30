6e7696a9376a0d0528c985dced6622e29a13a786
yagorodrigues2023 <yagorodrigues.contato@gmail.com>
2025-12-30 15:24:07 -0300

    New Version

--- src/pages/app/index.jsx
+++ src/pages/app/index.jsx
@@ -1,20 +1,17 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Route , Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
 import Login from '../login';
-//import Home
 import ResetPass from '../resetpass';
 
-export default function App(){
-    
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/reset-password" element={<ResetPass />} />
-                <Route path="*" element={<Navigate to="/" />} />
-            </Routes>
-        </Router>
-
-    )   
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/reset-password" element={<ResetPass />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
+  );
 }
