b2040c1f53661af568c0f511a30f4a32ded1b0d7
yagorodrigues2023 <yagorodrigues.contato@gmail.com>
2025-12-30 15:56:26 -0300

    New Version

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -8,7 +8,7 @@ export default function Login(){
     
     return (
     <div className={`${styles.container} ${styles.no_select}`}>
-        <img src={logo} className={styles.logo} width='100%' style={{marginBottom: '1rem', marginTop: '3rem'}} />
+        <img src={logo} className={styles.logo} width='100%' height='auto'  />
         <FormLogin  />
     </div>
     )   
